0e3edb2efc04bff70ac9080c90abe791a9f8cc9d
Alexandr <isbarius@yandex.ru>
2023-10-22 18:04:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13676; // стоимость билета (руб)
+        int bonus = 20; // количество потраченных рублей за 1 милю
+
+        System.out.println("Количество бонусных миль");
+        System.out.println(amount / bonus);
+
+    }
+}
